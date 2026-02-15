d55d5c2f61d2f5505e2f9c5e552d07e9f9e9fed8
yamanmetehan00-crypto <yamanmetehan00@gmail.com>
2026-02-15 19:35:27 +0300

    Add Firebase and Cloudinary configuration
    Added Firebase configuration and Cloudinary settings.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,22 @@
+// js/firebase-config.js
+// BUNU KENDİ BİLGİLERİNLE DEĞİŞTİR!
+
+const firebaseConfig = {
+    apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "gundemx-news.firebaseapp.com",
+    databaseURL: "https://gundemx-news-default-rtdb.firebaseio.com",
+    projectId: "gundemx-news",
+    storageBucket: "gundemx-news.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
+};
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const haberlerRef = database.ref('haberler');
+
+// Cloudinary Config (Resim upload için)
+const CLOUDINARY_CLOUD_NAME = 'senin-cloud-adin'; // DEĞİŞTİR!
+const CLOUDINARY_UPLOAD_PRESET = 'gundemx_upload'; // DEĞİŞTİR!
 
